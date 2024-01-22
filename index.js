515851100047546636b14aff05c26ec23489dc84
bvnnybae <157345766+bvnnybae@users.noreply.github.com>
2024-01-22 15:09:09 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = [".gg/kuroscafe"];
 
 
 let currentIndex = 0;
